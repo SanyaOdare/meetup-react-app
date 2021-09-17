900646196e5ad48a9eb348536848d9b59aac7ccf
Sanya Oluwadare <sanyaodare@gmail.com>
2021-09-17 02:30:15 +0100

    import switch component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetups';
+import FavoritesPage from './pages/Favorite';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/">
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
