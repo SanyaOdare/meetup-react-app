fdbd09081743ce9fe2dd0e93777235c6abedb89e
Sanya Oluwadare <sanyaodare@gmail.com>
2021-09-18 21:40:23 +0100

    add keys to list

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetups';
 import FavoritesPage from './pages/Favorite';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
-        <Route path="/">
+        <Route path='/' exact>
           <AllMeetupsPage />
         </Route>
-        <Route path="/new-meetup">
+        <Route path='/new-meetup'>
           <NewMeetupPage />
         </Route>
-        <Route path="/favorites">
+        <Route path='/favorites'>
           <FavoritesPage />
         </Route>
       </Switch>
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,35 @@
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://images.pexels.com/photos/3201921/pexels-photo-3201921.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
+
 function AllMeetupsPage() {
-  return <div>All Meetups Page</div>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return <li key={meetup.id}>{meetup.title}</li>;
+        })}
+      </ul>
+    </section>    
+  );
 }
 
 export default AllMeetupsPage;
