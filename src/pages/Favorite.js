72a16a4d8b3d22426ca45d9bdc7e66cca0c4eda0
Sanya Oluwadare <sanyaodare@gmail.com>
2021-09-20 23:51:49 +0100

    add badge class

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -6,10 +6,18 @@ import MeetupList from '../components/meetups/MeetupList';
 function FavoritesPage() {
   const favoriteCtx = useContext(FavoritesContext);
 
+  let content;
+
+  if (favoriteCtx.totalFavorites === 0) {
+    content = <p>You've got no favorites yet. Start adding some?</p>
+  } else {
+    content = <MeetupList meetups={favoriteCtx.favorites} />
+  }
+
   return (
     <section>
       <h1>My Favorites</h1>;
-      <MeetupList meetups={favoriteCtx.favorites} />
+      {content}
     </section>
   );
 }
